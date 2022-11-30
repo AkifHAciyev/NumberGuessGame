67c46c9cfe654cd45c3340b0ade2ee57c2557e4b
AkifHaciyev <akif6563@mail.ru>
2022-11-30 14:56:22 +0400

    end

---
+++ index.js
@@ -0,0 +1,36 @@
+const button = document.getElementById('button');
+const myNumber = document.getElementById('myNumber');
+const guessedNum = document.getElementById('guessedNum');
+const numberOfGuesses = document.getElementById('numberOfGuesses');
+const highFew = document.getElementById('highFew');
+const error = document.getElementById('error');
+
+let randomNum = Math.floor(Math.random() * 15);
+console.log(randomNum);
+
+button.addEventListener('click', () => {
+	if (myNumber.value == randomNum) {
+		highFew.innerHTML = 'you are winner';
+	} else if (myNumber.value < randomNum) {
+		highFew.innerHTML = 'few';
+	} else if (myNumber.value > randomNum) {
+		highFew.innerHTML = 'high';
+	}
+
+	guessedNum.innerHTML = myNumber.value;
+
+	console.log(myNumber.value);
+
+	console.log(randomNum);
+
+	if (0 > myNumber.value || myNumber.value > 15) {
+		myNumber.value = 'password length must be between 1 to 15';
+		error.innerHTML = 'password length must be between 1 to 15';
+	}
+});
+
+let count = 0;
+button.onclick = function () {
+	count++;
+	numberOfGuesses.innerHTML = count;
+};
